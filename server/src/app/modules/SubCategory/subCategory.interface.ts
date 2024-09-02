9da4baafb53e548f4cb1da144bf4ebfc64c79bb1
Tajul Islam <mdtanvir7462@gmail.com>
2024-09-02 12:57:17 +0600

    tanvir :- working on subcategory modules

---
+++ server/src/app/modules/SubCategory/subCategory.interface.ts
@@ -0,0 +1,10 @@
+export interface ISubCategory {
+    name: string;
+    description?: String;
+    isActive?: boolean;
+    isDeleted?: boolean;
+    subCategoryImage: string;
+    categoryName?: string;
+    categoryId: string
+
+};
---
+++ server/src/app/modules/SubCategory/subCategory.model.ts
@@ -0,0 +1,45 @@
+import { Schema, model } from 'mongoose';
+import { ISubCategory } from './subCategory.interface';
+
+// subCategory schema
+const subCategorySchema = new Schema<ISubCategory>(
+    {
+        name: {
+            type: String,
+            required: [true, 'Subcategory name is required'],
+            trim: true,
+            unique: true,
+        },
+        description: {
+            type: String,
+            required: false,
+            trim: true,
+        },
+        isActive: {
+            type: Boolean,
+            required: false,
+            default: true
+        },
+        isDeleted: {
+            type: Boolean,
+            required: false,
+            default: false
+        },
+        subCategoryImage: {
+            type: String,
+            required: true,
+        },
+        categoryId: {
+            type: String,
+            required: true,
+        },
+        categoryName: {
+            type: String,
+            required: false,
+        },
+    },
+    { timestamps: true },
+);
+
+// category Model
+export const SubCategoryModel = model<ISubCategory>('Subcategory', subCategorySchema);
