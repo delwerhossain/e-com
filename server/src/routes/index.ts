a27107929af18c2ae6ddea4702e1ccd0e20e2cac
Tajul Islam <mdtanvir7462@gmail.com>
2024-09-03 17:38:22 +0600

    remove conflict

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,13 +1,6 @@
 import { Router } from 'express';
 import { categoryRoutes } from '../app/modules/Categories/categories.routes';
-<<<<<<< HEAD
-import { subcategoryRoutes } from '../app/modules/SubCategory/subCategory.routes';
-
-
-
-=======
 import { userRoutes } from '../app/modules/Users/users.routes';
->>>>>>> 2f7a7d79ef0dd64d21e0096a9ebb617e218e9830
 
 const router = Router();
 
@@ -17,11 +10,6 @@ const moduleRoutes = [
     route: categoryRoutes,
   },
   {
-<<<<<<< HEAD
-    path: '/subcategories',
-    route:subcategoryRoutes ,
-  }
-=======
     path: '/sub',
     route: categoryRoutes,
   },
@@ -29,7 +17,6 @@ const moduleRoutes = [
     path: '/user',
     route: userRoutes,
   },
->>>>>>> 2f7a7d79ef0dd64d21e0096a9ebb617e218e9830
 ];
 
 moduleRoutes.forEach(route => router.use(route.path, route.route));
