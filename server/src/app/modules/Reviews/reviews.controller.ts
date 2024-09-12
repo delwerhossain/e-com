a652e06f5458f79c2e55ea16facf47310a7590ec
MD-TANVIR7462 <mdtanvir7462@gmail.com>
2024-09-12 15:44:36 +0600

    setup after merge

--- server/src/app/modules/Reviews/reviews.controller.ts
+++ server/src/app/modules/Reviews/reviews.controller.ts
@@ -3,15 +3,15 @@ import reviewsValidation from './reviews.validation';
 import { ReviewServices } from './reviews.services';
 
 const createReview: RequestHandler = async (req, res, next) => {
-    try {
-        const review = reviewsValidation.parse(req.body);
-        const result = await ReviewServices.createReview(review);
-        res.status(201).json({
-            success: true,
-            message: 'Review created successfully',
-            data: result,
-        });
-    } catch (error) {
-        next(error);
-    }
+  try {
+    const review = reviewsValidation.parse(req.body);
+    const result = await ReviewServices.createReview(review);
+    res.status(201).json({
+      success: true,
+      message: 'Review created successfully',
+      data: result,
+    });
+  } catch (error) {
+    next(error);
+  }
 };
